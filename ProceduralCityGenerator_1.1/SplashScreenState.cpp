6e5b48c612efce189a826fc30e1b08bcf8b118a6
klawson147 <kelbylawson@gmail.com>
2017-06-21 02:35:37 -0400

    texture + sizing

--- ProceduralCityGenerator_1.1/SplashScreenState.cpp
+++ ProceduralCityGenerator_1.1/SplashScreenState.cpp
@@ -2,23 +2,55 @@
 
 SplashScreenState::SplashScreenState(StateManager* stateManager) : IState(stateManager)
 {
-
 }
 
 void SplashScreenState::Init()
 {
 	m_title_main.setFont(m_s_c_resourceHolder.fonts.get(FontName::sf_atarian_system));
 	m_title_main.setCharacterSize(62);
-	m_title_main.setColor(sf::Color(255, 255, 255, 255));
+	m_title_main.setColor(sf::Color(0, 0, 0, 255));
 	m_title_main.setString("Procedural City Generator");
-	m_title_main.setPosition(300, 300);
 
+	sf::Vector2f pos = DisplayWindow::getRelativePosition(
+		DisplayWindowPosition::top_center,
+		sf::Vector2u(
+		m_title_main.getGlobalBounds().width,
+		m_title_main.getGlobalBounds().height),
+		sf::Vector2f(0, 0));
 
+	m_title_main.setPosition(pos);
 
 	m_title_secondary.setFont(m_s_c_resourceHolder.fonts.get(FontName::sf_atarian_system));
+	m_title_secondary.setCharacterSize(36);
+	m_title_secondary.setColor(sf::Color(0, 0, 0, 255));
+	m_title_secondary.setString("Created Using");
 
-	m_logo.setTexture(m_s_c_resourceHolder.textures.get(TextureName::sfml_logo));
-	m_background.setTexture(m_s_c_resourceHolder.textures.get(TextureName::skyline1920));
+	pos = DisplayWindow::getRelativePosition(
+		DisplayWindowPosition::top_center,
+		sf::Vector2u(
+		m_title_secondary.getGlobalBounds().width,
+		m_title_secondary.getGlobalBounds().height),
+		sf::Vector2f(0, 125));
+
+	m_title_secondary.setPosition(pos);
+
+	m_logo.setTexture(m_s_c_resourceHolder.textures.get(TextureName::sfml_logo_small));
+	m_logo.setPosition(
+		DisplayWindow::getRelativePosition(
+		DisplayWindowPosition::center_center,
+		sf::Vector2u(
+		m_logo.getLocalBounds().width,
+		m_logo.getLocalBounds().height),
+		sf::Vector2f(0, -50)));
+
+		m_background.setTexture(m_s_c_resourceHolder.textures.get(TextureName::skyline1920));
+		m_background.setPosition(
+			DisplayWindow::getRelativePosition(
+			DisplayWindowPosition::bottom_left,
+			sf::Vector2u(
+			m_background.getLocalBounds().width,
+			m_background.getLocalBounds().height),
+			sf::Vector2f(-20, 20)));
 }
 
 void SplashScreenState::Cleanup()
@@ -39,13 +71,12 @@ void SplashScreenState::GetEvents()
 
 void SplashScreenState::Update()
 {
-
 }
 
 void SplashScreenState::Display()
 {
 	DisplayWindow::Draw(m_title_main);
-	//DisplayWindow::Draw(m_title_secondary);
-	//DisplayWindow::Draw(m_logo);
-	//DisplayWindow::Draw(m_background);
+	DisplayWindow::Draw(m_title_secondary);
+	DisplayWindow::Draw(m_logo);
+	DisplayWindow::Draw(m_background);
 }
